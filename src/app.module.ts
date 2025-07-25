8bd78ba2b20a8b98e372f62c41a39b640cbeb253
stalincrisanto <stalin.crisanto@metrodequito.gob.ec>
2025-07-25 14:07:00 -0500

    feat: initial config and and libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "./infraestructure/shared/prisma/prisma.module";
+import { DepartmentModule } from "./interfaces/modules/DepartmentModule";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    DepartmentModule
+  ],
 })
 export class AppModule {}
